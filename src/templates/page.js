1bcaee9736fc26c2ee43a3dd90bcd4a94fa9058c
Nick McNeany <nmcneany@gmail.com>
2020-01-20 13:09:05 -0600

    create main-menu component

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import Layout from '../components/Layout'
 export default ({pageContext}) => (
-    <div>
+    <Layout>
         <h1>{pageContext.title}</h1>
         <p>{pageContext.content}</p>
-    </div>
+    </Layout>
 )
