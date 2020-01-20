69b402728ce6caa8f393d19ff089944637dbb0ce
Nick McNeany <nmcneany@gmail.com>
2020-01-20 14:53:17 -0600

    dangerously set html for the_content

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+
 export default ({pageContext}) => (
     <Layout>
         <h1>{pageContext.title}</h1>
-        <p>{pageContext.content}</p>
+        <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
     </Layout>
 )
